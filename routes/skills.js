f619f3be1b99294af1c3b2300728580756e435c6
Jermaine <jermzblake@gmail.com>
2020-10-19 10:02:35 -0400

    adjust routing to properly show edit view

--- routes/skills.js
+++ routes/skills.js
@@ -7,7 +7,7 @@ const skillsCtrl = require('../controllers/skills')
 router.get('/', skillsCtrl.index);
 router.post('/', skillsCtrl.create);
 router.get('/new', skillsCtrl.new)
-router.get('/edit', skillsCtrl.edit);
+router.get('/:id/edit', skillsCtrl.edit);
 router.get('/:id', skillsCtrl.show);
 router.delete('/:id', skillsCtrl.delete);
 router.put('/', skillsCtrl.update);
